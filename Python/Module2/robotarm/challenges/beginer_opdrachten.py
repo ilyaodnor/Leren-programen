d16730623f404caad1b2cd9310968368cc12d36b
Ваше имя <99083003@mydavinci.nl>
2024-09-29 15:30:57 +0200

    beginer 3

--- Python/Module2/robotarm/challenges/beginer_opdrachten.py
+++ Python/Module2/robotarm/challenges/beginer_opdrachten.py
@@ -2,16 +2,10 @@ import sys
 sys.path.insert(0,'/Users/ilya/Documents/GitHub/School/Softwere dev./python/Leren-programen/Python/Module2/robotarm')
 from RobotArm import RobotArm
 from beginner import challenges
-robotArm = RobotArm(challenges[2],3)
-robotArm.moveRight()
-for i in range(6):
+robotArm = RobotArm(challenges[3],3)
+for i in range(5):
     robotArm.grab()
-    if robotArm.scan() == 'blue':
-        robotArm.moveLeft()
-        robotArm.drop()
-        robotArm.moveRight()
-    else:
-        robotArm.moveRight()
-        robotArm.drop()
-        robotArm.moveLeft()
+    for i in range(4): robotArm.moveRight()
+    robotArm.drop()
+    for i in range(4): robotArm.moveLeft()
 robotArm.report()
