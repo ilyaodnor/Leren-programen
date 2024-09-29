20315dc06dfa28a4af01f2427356f0e4508e2452
Ваше имя <99083003@mydavinci.nl>
2024-09-29 15:46:40 +0200

    beginer5

--- Python/Module2/robotarm/challenges/beginer_opdrachten.py
+++ Python/Module2/robotarm/challenges/beginer_opdrachten.py
@@ -2,15 +2,12 @@ import sys
 sys.path.insert(0,'/Users/ilya/Documents/GitHub/School/Softwere dev./python/Leren-programen/Python/Module2/robotarm')
 from RobotArm import RobotArm
 from beginner import challenges
-robotArm = RobotArm(challenges[4],3)
+robotArm = RobotArm(challenges[5],3)
 robotArm.moveRight()
-for i in range(3):
+for i in range(7):
     robotArm.grab()
-    robotArm.moveLeft()
+    for b in range(8): robotArm.moveRight()
     robotArm.drop()
-    robotArm.moveRight()
-    robotArm.grab()
-    robotArm.moveRight()
-    robotArm.drop()
-    if i != 2: robotArm.moveLeft()
+    if i != 6:
+        for d in range(8): robotArm. moveLeft()
 robotArm.report()
