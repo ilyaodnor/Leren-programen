e7900a96cf77d412ef05ca5090a03fb421e81e1e
Ваше имя <99083003@mydavinci.nl>
2024-09-29 20:28:06 +0200

    medium 2

--- Python/Module2/robotarm/challenges/medium_opdrachten.py
+++ Python/Module2/robotarm/challenges/medium_opdrachten.py
@@ -2,14 +2,14 @@ import sys
 sys.path.insert(0,'/Users/ilya/Documents/GitHub/School/Softwere dev./python/Leren-programen/Python/Module2/robotarm')
 from RobotArm import RobotArm
 from medium import challenges
-robotArm = RobotArm(challenges[1],3)
-for i in range(8): robotArm.moveRight()
-for j in range(9):
-    robotArm.grab()
-    for d in range(1): robotArm.moveRight()
-    robotArm.drop()
-    if j!= 8:
-        for b in range (2): robotArm.moveLeft()
-    d+=1
-    b+=1
+robotArm = RobotArm(challenges[2],3)
+a = 2
+for i in range(5):
+    for j in range(6):
+            robotArm.moveRight()
+            robotArm.grab()
+            robotArm.moveLeft()
+            robotArm.drop()
+    for d in range(a):
+        if i!=4 :robotArm.moveRight()
 robotArm.report()
