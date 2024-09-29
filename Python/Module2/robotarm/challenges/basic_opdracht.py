31ca95157c666408aee876b84c154d9ba97645fa
Ваше имя <99083003@mydavinci.nl>
2024-09-29 15:08:37 +0200

    basic 5

--- Python/Module2/robotarm/challenges/basic_opdracht.py
+++ Python/Module2/robotarm/challenges/basic_opdracht.py
@@ -2,16 +2,15 @@ import sys
 sys.path.insert(0,'/Users/ilya/Documents/GitHub/School/Softwere dev./python/Leren-programen/Python/Module2/robotarm')
 from RobotArm import RobotArm
 from basic import challenges
-robotArm = RobotArm(challenges[4],3)
+robotArm = RobotArm(challenges[5],3)
+robotArm.moveRight()
 robotArm.grab()
-for i in range(9): robotArm.moveRight()
-robotArm.drop()
-for i in range(5): robotArm.moveLeft()
-robotArm.grab()
-for i in range(5): robotArm.moveRight()
-robotArm.drop()
-for i in range(2): robotArm.moveLeft()
-robotArm.grab()
-for i in range (2): robotArm.moveRight()
-robotArm.drop()
+robotArm.scan()
+if robotArm.scan() == 'red': 
+    robotArm.moveLeft()
+    robotArm.drop()
+elif robotArm.scan()== 'yellow':
+    robotArm.moveRight()
+    robotArm.drop()
 robotArm.report()
+robotArm.help()
