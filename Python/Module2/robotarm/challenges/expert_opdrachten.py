c78539d2bd4661684ad6e562a2e7aa5b0d58eedf
Ваше имя <99083003@mydavinci.nl>
2024-10-02 09:36:45 +0200

    expert 2

--- Python/Module2/robotarm/challenges/expert_opdrachten.py
+++ Python/Module2/robotarm/challenges/expert_opdrachten.py
@@ -2,21 +2,20 @@ import sys
 sys.path.insert(0,'/Users/ilya/Documents/GitHub/School/Softwere dev./python/Leren-programen/Python/Module2/robotarm')
 from RobotArm import RobotArm
 from expert import challenges
-robotArm = RobotArm(challenges[1],2)
+robotArm = RobotArm(challenges[2],3)
 aantaal = 1
-for i in range(4):
-        for j in range(aantaal):
-            robotArm.grab()
-            for k in range(5): robotArm.moveRight()
-            robotArm.drop()
-            if j !=aantaal-1:
-                  for k in range(5):
-                        robotArm.moveLeft()
-            else:
-                  for k in range(4):
-                        robotArm.moveLeft()
-        if i == 4:
-              break
-        else:
-              aantaal+=1
+for i in range(9):
+    robotArm.grab()
+    if robotArm.scan() == 'red':
+        while robotArm.stackIndex()!=9:
+            robotArm.moveRight()
+            aantaal+=1
+        robotArm.drop()
+        for i in range(aantaal-1):
+            print(aantaal)
+            robotArm.moveLeft()
+    else:
+        robotArm.drop()
+        robotArm.moveRight()   
+    aantaal=0
 robotArm.report()
