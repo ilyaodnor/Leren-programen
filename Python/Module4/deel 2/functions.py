91fc12433fe4e306de0f852161e1b2f488617773
Ваше имя <99083003@mydavinci.nl>
2024-10-09 22:05:54 +0200

    deel 2 module 4

---
+++ Python/Module4/deel 2/functions.py
@@ -0,0 +1,16 @@
+def addition(num1:float, num2:float):
+    return num1 + num2
+def substracrion(num1:float,num2:float):
+    return num1 - num2
+def multiplication(num1:float,num2:float):
+    return num1*num2
+def division(num1:float,num2:float):
+    return num1/num2
+def add(num1:float):
+    return num1 + 1
+def sub(num1:float):
+    return num1-1
+def mult(num1: float):
+    return num1*2
+def div(num1:float):
+    return num1/2
